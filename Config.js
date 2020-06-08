4b78a7b57cb4b6bf76e3e81b1b12c6b64462e4cb
HiroDev-0016 <57238819+HiroDev-0016@users.noreply.github.com>
2020-06-08 18:15:55 -0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,8 @@
+
+exports.TOKEN = ''; //THIS BOT IS FOR MULTIPLE GUILD
+exports.PREFIX = '';
+
+exports.GOOGLE_API_KEY = 'AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w';
+exports.COLOR = '#fffff'; //COLOR OF EMBED
+exports.STATUS = `🎶 Music Forever! Use *play e o show vai começar!`; //ADD YOUR BOT STATUS
+
